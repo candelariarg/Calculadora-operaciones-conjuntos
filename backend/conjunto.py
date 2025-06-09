a3c6166722e04e433061fa24fdd7f1409d3b4f74
Nyx Paez <margotpaez21@gmail>
2025-06-09 14:45:48 -0300

    cambie la función de conjuntos

--- backend/conjunto.py
+++ backend/conjunto.py
@@ -1,16 +1,7 @@
 def obtener_conjuntos():
-    while True:
-        try:
-            numero_conjuntos = int(input("Ingrese el número de conjuntos con los que desea trabajar (2 o 3): "))
-            if numero_conjuntos in [2, 3]:
-                break
-            else:
-                print("Por favor, ingrese 2 o 3.")
-        except ValueError:
-            print("Entrada inválida. Por favor, ingrese un número.")
-
+    print("Trabajará con 2 conjuntos.")
     conjuntos = []
-    for i in range(1, numero_conjuntos + 1):
+    for i in range(1, 3):
         conjunto = input(f"Ingrese el conjunto {i} (ej. 'a,b,c'): ")
         conjuntos.append([elem.strip() for elem in conjunto.split(",")])
     return conjuntos
